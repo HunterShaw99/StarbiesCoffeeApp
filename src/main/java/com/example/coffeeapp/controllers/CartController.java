87269299868068f0b17e9e02da2290054f5cb3f0
Hunter Shaw <hms75@pitt.edu>
2022-04-03 19:57:16 -0400

    Added cart total label and utilized observer pattern to update cart total label accordingly

--- src/main/java/com/example/coffeeapp/controllers/CartController.java
+++ src/main/java/com/example/coffeeapp/controllers/CartController.java
@@ -2,6 +2,7 @@ package com.example.coffeeapp.controllers;
 
 import com.example.coffeeapp.data.CartManager;
 import com.example.coffeeapp.data.models.CoffeeModel;
+import com.example.coffeeapp.observer.Observer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -9,13 +10,17 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
-public class CartController {
+import java.math.BigDecimal;
+
+import static com.example.coffeeapp.data.models.constants.CoffeePrice.MEDIUM_COST;
+
+public class CartController implements Observer {
 
     @FXML
     private HBox bottomHBox;
 
     @FXML
-    private Label menuLabel;
+    private Label menuLabel, totalLabel, totalValueLabel;
 
     @FXML
     private ListView<CoffeeModel> menuListView;
@@ -26,9 +31,12 @@ public class CartController {
     private byte init = 0;
 
     public void initialize() {
+
         if (init == 0) {
             menuListView.setItems(CartManager.GetInstance().GetCartItems());
             menuListView.setCellFactory(new CartCellFactory());
+            totalValueLabel.setText("");
+            CartManager.GetInstance().registerObserver(this);
             init++;
         }
     }
@@ -37,9 +45,15 @@ public class CartController {
     void AddBevCart(MouseEvent event) {
         CoffeeModel m = new CoffeeModel();
         m.setName("Cold Brew a");
+        m.setPrice(MEDIUM_COST);
         CartManager.GetInstance().AddBeverage(m);
         System.out.println(m);
     }
 
 
+    @Override
+    public void update(BigDecimal total) {
+        //Will update the cart total here when an item is added/removed in CartManager class
+        totalValueLabel.setText("$"+total);
+    }
 }
