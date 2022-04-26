0edb88ee9b8c6be3a4d364eaaacb294a8fa26d91
Hunter Shaw <HMS75@pitt.edu>
2022-04-25 23:15:48 -0400

    Have the coffeeapp connect to server using NetworkManager

--- src/main/java/com/example/coffeeapp/CoffeeApp.java
+++ src/main/java/com/example/coffeeapp/CoffeeApp.java
@@ -1,16 +1,28 @@
 package com.example.coffeeapp;
 
 import com.example.coffeeapp.controllers.ControllerHandler;
+import com.example.coffeeapp.data.NetworkManager;
 import com.example.coffeeapp.tasks.ViewChangeTask;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class CoffeeApp extends Application {
+
+    static final int port = 5001;
     @Override
     public void start(Stage stage) throws IOException {
+        InetAddress addr =
+                InetAddress.getByName("localhost");
+        System.out.println("Server address = " + addr);
+        // Set up socket on client side that connects to server
+        Socket socket = new Socket(addr, port);
+        System.out.println("Connected to socket: " + socket);
+        NetworkManager.getInstance().setClient(socket);
         stage.setTitle("Stardoes Coffee");
         ControllerHandler.GetInstance().SetStage(stage);
         Platform.runLater(new ViewChangeTask(4));
---
+++ src/main/java/com/example/coffeeapp/data/NetworkManager.java
@@ -0,0 +1,56 @@
+package com.example.coffeeapp.data;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class NetworkManager {
+
+
+    private volatile static NetworkManager instance = null;
+
+    private volatile static Socket client;
+
+    private Object data[];
+
+    private static ObjectInputStream oiStream;
+    private static ObjectOutputStream ooStream;
+
+    private NetworkManager() {
+        //Sets a Object array to size 2, in case we want to keep things simple and just use this for data transfer
+        data = new Object[2];
+    }
+
+    public static NetworkManager getInstance() {
+        if (instance == null) {
+            synchronized (CoffeeManager.class) {
+                if (instance == null) {
+                    instance = new NetworkManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private void setOiStream() throws IOException {
+        oiStream = new ObjectInputStream(client.getInputStream());
+    }
+
+    private void setOoStream() throws IOException {
+        ooStream = new ObjectOutputStream(client.getOutputStream());
+    }
+
+    public void setClient(Socket client) throws IOException {
+        this.client = client;
+        System.out.println("Set Client to "+ this.client);
+        //Could have potentially swapped these around. Might be that the oiStream has to be clients outputstream
+        // and ooStream has to be connected to the client.getInputStream.
+        setOiStream(); //Set the ObjectInput stream
+        setOoStream(); //Set the ObjectOutput stream
+    }
+
+    public void closeConnection() throws IOException {
+        client.close();
+    }
+}
